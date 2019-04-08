0222e3ab71e617e8d876506421e9310f8110ba2e
unknown <sahmad97@uw.edu>
2019-04-07 23:47:38 -0700

    Updated the build.

---
+++ First/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	
+	private Rigidbody rb;
+	public int Speed;
+
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+	}
+	// Check for input and apply force
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal * Speed, 0.0f, moveVertical * Speed);
+		rb.AddForce(movement);
+	}
+}
